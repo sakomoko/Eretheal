232f4e7ca71f402312a305bce1e550b747de65aa
AIZAWA Sako <sakomoko@gmail.com>
2011-08-25 02:59:25 +0900

    add StatusAdjustmentClass

---
+++ app/models/status_adjustment.rb
@@ -0,0 +1,22 @@
+class StatusAdjustment
+  include Mongoid::Document
+
+  embedded_in :skill
+
+  field :dmg, :type => Integer, :default => 0
+  field :def, :type => Integer, :default => 0
+  field :hit, :type => Integer, :default => 0
+  field :avoid, :type => Integer, :default => 0
+  field :magi, :type => Integer, :default => 0
+  field :magi_def, :type => Integer, :default => 0
+
+  field :dex, :type => Integer, :default => 0
+  field :agi, :type => Integer, :default => 0
+  field :int, :type => Integer, :default => 0
+  field :vit, :type => Integer, :default => 0
+  field :str, :type => Integer, :default => 0
+  field :mnd, :type => Integer, :default => 0
+
+  field :hp, :type => Integer, :default => 0
+  field :mp, :type => Integer, :default => 0
+end
