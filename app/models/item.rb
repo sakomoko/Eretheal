71424ebfab5b27b3168386b53885e0c5ad0c75d9
AIZAWA Sako <sakomoko@gmail.com>
2011-08-24 22:16:24 +0900

    add default value

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,32 +6,32 @@ class Item
   belongs_to :item_type
 
   field :name, :type => String
-  field :power, :type => Integer
-  field :speed, :type => Integer
-  field :weight, :type => Integer
+  field :power, :type => Integer, :default => 0
+  field :speed, :type => Integer, :default => 0
+  field :weight, :type => Integer, :default => 0
   field :color, :type => String
-  field :hit, :type => Integer
-  field :avoid, :type => Integer
-  field :magi, :type => Integer
-  field :magi_def, :type => Integer
+  field :hit, :type => Integer, :default => 0
+  field :avoid, :type => Integer, :default => 0
+  field :magi, :type => Integer, :default => 0
+  field :magi_def, :type => Integer, :default => 0
 
-  field :add_dex, :type => Integer
-  field :add_agi, :type => Integer
-  field :add_vit, :type => Integer
-  field :add_str, :type => Integer
-  field :add_int, :type => Integer
-  field :add_mnd, :type => Integer
+  field :add_dex, :type => Integer, :default => 0
+  field :add_agi, :type => Integer, :default => 0
+  field :add_vit, :type => Integer, :default => 0
+  field :add_str, :type => Integer, :default => 0
+  field :add_int, :type => Integer, :default => 0
+  field :add_mnd, :type => Integer, :default => 0
 
-  field :add_hp, :type => Integer
-  field :add_hp, :type => Integer
+  field :add_hp, :type => Integer, :default => 0
+  field :add_hp, :type => Integer, :default => 0
 
   field :two_handed, :type => Boolean, :default => 0
 
   field :explain, :type => String
-  field :critical, :type => Integer
+  field :critical, :type => Integer, :default => 0
 
   field :stack, :type => Boolean, :default => 1
-  field :price, :type => Integer
+  field :price, :type => Integer, :default => 0
 
   field :demand, :type => Integer, :default => 5
 
