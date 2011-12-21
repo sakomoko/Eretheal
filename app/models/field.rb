f322f66c269c704b856fba4791034d851dd16775
sakomoko <sakomoko@gmail.com>
2011-12-21 17:55:18 +0900

    add Field seeds data.

--- app/models/field.rb
+++ app/models/field.rb
@@ -6,6 +6,6 @@ class Field
   belongs_to :link, :class_name => 'Field'
 
   field :name, :type => String
-  field :no_image, :type => Boolean
-  field :distance, :type => Integer
+  field :no_image, :type => Boolean, :default => true
+  field :distance, :type => Integer, :default => 0
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +6,30 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+module Eretheal
+  module Seed
+    module Field
+      class << self
+        def create(fields, parent = nil)
+          fields.each do |field|
+            childrens = nil
+            if field.key? "children"
+              childrens = field["children"]
+              field.delete "children"
+            end
+            field[:parent] = parent if parent
+            m = ::Field.new field
+            m.id = field["id"]
+            m.save!
+            create childrens, field["id"] if childrens
+          end
+        end
+      end
+    end
+  end
+end
+
+fields = YAML.load_file "#{Rails.root}/db/seeds/fields.yml"
+Field.delete_all
+Eretheal::Seed::Field.create fields
