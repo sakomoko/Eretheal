230f8a5897f4c551d646079633df747c4e453fdb
AIZAWA Sako <sakomoko@gmail.com>
2013-01-01 09:53:08 +0900

    Messageモデルにslugを追加。

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,9 +1,13 @@
 class Message
   include Mongoid::Document
+  include Mongoid::Slug
 
-  field :key, :type => String
-  field :message, :type => String
-  field :level, :type => Integer, :default => 1
-  field :class, :type => String
+  field :key, type: String
+  field :body, type: String
+  field :level, type: Integer, default: 1
 
+  slug :key
+
+  validates_uniqueness_of :key
+  validates_presence_of :key
 end
