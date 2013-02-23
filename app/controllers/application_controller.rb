cec8ce6c5dec24d79fb487bb59e732d31f490d40
sakomoko <sakomoko@gmail.com>
2013-02-24 04:35:16 +0900

    jsonでのリクエスト時にCSRFトークンを求めないようにした。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
   alias :login_required :authenticate_user!
   alias :logged_in? :user_signed_in?
+  respond_to :html, :json
 
   def current_user=(user)
     sign_in(user)
