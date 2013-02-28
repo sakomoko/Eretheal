20cf14c968fef00ab4ebebbe336b195405cec242
sakomoko <sakomoko@gmail.com>
2013-02-28 11:50:23 +0900

    spec_helperを修正。

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -36,11 +36,16 @@ Spork.prefork do
     # Clean up the database
     require 'database_cleaner'
     config.before(:suite) do
-      DatabaseCleaner.strategy = :truncation
       DatabaseCleaner.orm = "mongoid"
+      DatabaseCleaner.strategy = :truncation
+      DatabaseCleaner.clean_with(:truncation)
     end
 
     config.before(:each) do
+      DatabaseCleaner.start
+    end
+
+    config.after(:each) do
       DatabaseCleaner.clean
     end
   end
