b9a80624fb40f93ff669b39ff83e699c1c15e84d
sakomoko <sakomoko@gmail.com>
2013-10-26 04:28:27 +0900

    フィールドを変更。

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,17 +1,16 @@
 class Skill
   include Mongoid::Document
-  include Mongoid::Paranoia
-  include Mongoid::Timestamps
-
+  include Eretheal::Seeder
 
   embeds_one :status_adjustment
   embeds_one :status_coefficient
 
   accepts_nested_attributes_for :status_adjustment, :status_coefficient
 
-  field :key, :type => String
+  field :_id, type: String, default: -> { key }
+  field :key, :type => Symbol
   field :name, :type => String
   field :cost, :type => Integer, :default => 0
-  field :explain, :type => String
+  field :description, :type => String
 
 end
